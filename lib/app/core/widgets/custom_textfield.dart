121e219712e02a7d286e50425b2e0cde77f1dca5
rizaadi <rizaadi890@gmail.com>
2022-06-19 11:53:58 +0700

    tambah kpi fix

--- lib/app/core/widgets/custom_textfield.dart
+++ lib/app/core/widgets/custom_textfield.dart
@@ -4,21 +4,23 @@ import 'package:flutter/material.dart';
 class CustomTextField extends StatelessWidget {
   const CustomTextField({
     Key? key,
+    required this.label,
   }) : super(key: key);
+
+  final String label;
+// final String hint;
 
   @override
   Widget build(BuildContext context) {
     return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-      const Text("Email"),
+      Text(label),
       TextField(
         decoration: InputDecoration(
           enabledBorder: OutlineInputBorder(
-              borderSide:
-                  const BorderSide(width: 1, color: Colors.black),
+              borderSide: const BorderSide(width: 1, color: Colors.black),
               borderRadius: BorderRadius.circular(6)),
           focusedBorder: OutlineInputBorder(
-              borderSide:
-                  const BorderSide(width: 1, color: Colors.black),
+              borderSide: const BorderSide(width: 1, color: Colors.black),
               borderRadius: BorderRadius.circular(6)),
         ),
       ),
--- lib/app/modules/tambah_kpi/views/tambah_kpi_view.dart
+++ lib/app/modules/tambah_kpi/views/tambah_kpi_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 import 'package:get/get.dart';
 import 'package:kpimobile/app/core/theme/theme_config.dart';
@@ -17,16 +18,47 @@ class TambahKpiView extends GetView<TambahKpiController> {
         ),
         body: SafeArea(
             child: SingleChildScrollView(
+          padding: EdgeInsets.all(16),
           child:
               Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
             Text(
               "Tambah KPI",
               style: Get.textTheme.headline5,
             ),
-            CustomTextField(),
-            CustomTextField(),
-            CustomTextField(),
-            CustomTextField(),
+            const SizedBox(
+              height: 16,
+            ),
+            const CustomTextField(label: "Deksripsi"),
+            const SizedBox(height: 12),
+            const CustomTextField(label: "Rumus"),
+            const SizedBox(height: 12),
+            const CustomTextField(label: "Bobot (%)"),
+            const SizedBox(height: 12),
+            const CustomTextField(label: "Target"),
+            const SizedBox(height: 12),
+            const CustomTextField(label: "Satuan"),
+            const SizedBox(height: 12),
+            const CustomTextField(label: "Sumber Data"),
+            const SizedBox(height: 12),
+            const CustomTextField(label: "Perlu Perhatian"),
+            const SizedBox(height: 12),
+            const CustomTextField(label: "Nilai 4"),
+            const SizedBox(height: 12),
+            const CustomTextField(label: "Nilai 3"),
+            const SizedBox(height: 12),
+            const CustomTextField(label: "Nilai 2"),
+            const SizedBox(height: 12),
+            const CustomTextField(label: "Nilai 1"),
+            const SizedBox(height: 12),
+            ElevatedButton.icon(
+                onPressed: () {},
+                icon: SvgPicture.asset(
+                  'assets/icons/save.svg',
+                  color: Colors.white,
+                  width: 24,
+                  height: 24,
+                ),
+                label: const Text("Simpan")),
           ]),
         )));
   }
